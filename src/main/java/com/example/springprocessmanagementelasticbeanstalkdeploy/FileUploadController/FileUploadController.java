4d164266c16b2e67b73ad13e7c5a5ba891174556
junxian428 <wengyin777@gmail.com>
2023-08-28 15:50:30 +0800

    deploy

--- src/main/java/com/example/springprocessmanagementelasticbeanstalkdeploy/FileUploadController/FileUploadController.java
+++ src/main/java/com/example/springprocessmanagementelasticbeanstalkdeploy/FileUploadController/FileUploadController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,12 +23,16 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileUploadController {
 
+    
     @GetMapping("/")
     public String showUploadForm( Model model) {
                 // Retrieve and display the list of files in the current directory
         List<String> fileList = listFilesInCurrentDirectory();
         List<String> highlightedLines = new ArrayList<>();
         List<String> highlightedLines_NODEJS = new ArrayList<>();
+        //sudo service mysql status
+
+
 
         model.addAttribute("fileList", fileList);
 
@@ -104,12 +109,57 @@ public class FileUploadController {
         }
 
         //
+        /*
+         * 
+         * 
+         * 
+         */
+
+
+        ProcessBuilder processBuilder_4 = new ProcessBuilder("sudo", "nmap", "localhost");
+        String port_status = "";
+
+         try {
+            Process process_4 = processBuilder_4.start();
+            int exitCode = process_4.waitFor();
+
+            if (exitCode == 0) {
+                port_status = readOutput(process_4.getInputStream());
+            } else {
+                port_status = "Port scan error.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred while retrieving Apache2 status.";
+        }
+
+         //sudo service mysql status
+
+        ProcessBuilder processBuilder_5 = new ProcessBuilder("sudo", "service", "mysql", "status");
+        String database_status = "";
+
+         try {
+            Process process_5 = processBuilder_5.start();
+            int exitCode = process_5.waitFor();
+
+            if (exitCode == 0) {
+                database_status = readOutput(process_5.getInputStream());
+            } else {
+                  database_status = "Database status error.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred while retrieving database status.";
+        }
+        //
         model.addAttribute("processInfo", processOutput.toString());
         model.addAttribute("highlightedLines", highlightedLines);
         model.addAttribute("highlightedLines_NODEJS",    highlightedLines_NODEJS);
         model.addAttribute("apacheStatus",    apache2_status);
         model.addAttribute("nginxStatus",    nginx_status);
+        model.addAttribute("portStatus",    port_status);
 
+        model.addAttribute("databaseStatus",    database_status);
 
 
 
@@ -190,7 +240,10 @@ public class FileUploadController {
         // Retrieve and display the list of files in the current directory
         List<String> fileList = listFilesInCurrentDirectory();
         model.addAttribute("fileList", fileList);
+        List<String> highlightedLines = new ArrayList<>();
+        List<String> highlightedLines_NODEJS = new ArrayList<>();
 
+        // Get process
         ProcessBuilder processBuilder = new ProcessBuilder("ps", "aux");
         StringBuilder processOutput = new StringBuilder();
 
@@ -200,6 +253,12 @@ public class FileUploadController {
 
             String line;
             while ((line = reader.readLine()) != null) {
+                if (line.contains("java -jar")) {
+                    highlightedLines.add(line);
+                } 
+                if (line.contains("node")) {
+                     highlightedLines_NODEJS.add(line);
+                }            
                 processOutput.append(line).append("\n");
             }
 
@@ -209,13 +268,111 @@ public class FileUploadController {
             e.printStackTrace();
             processOutput.append("Error occurred: ").append(e.getMessage());
         }
+        // Apache Status 
 
+        /*
+         * 
+         * 
+         * 
+         */
+        ProcessBuilder processBuilder_2 = new ProcessBuilder("service", "apache2", "status");
+        String apache2_status = "";
+
+         try {
+            Process process_2 = processBuilder_2.start();
+            int exitCode = process_2.waitFor();
+
+            if (exitCode == 0) {
+                apache2_status = readOutput(process_2.getInputStream());
+            } else {
+                apache2_status = "Apache2 service is not running or encountered an error.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred while retrieving Apache2 status.";
+        }
+        /*
+         * 
+         * 
+         * 
+         */
+
+        ProcessBuilder processBuilder_3 = new ProcessBuilder("systemctl", "status", "nginx");
+        String nginx_status = "";
+
+         try {
+            Process process_3 = processBuilder_3.start();
+            int exitCode = process_3.waitFor();
+
+            if (exitCode == 0) {
+                nginx_status = readOutput(process_3.getInputStream());
+            } else {
+                nginx_status = "Nginx service is not running or encountered an error.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred while retrieving Apache2 status.";
+        }
+
+        //
+        /*
+         * 
+         * 
+         * 
+         */
+
+
+        ProcessBuilder processBuilder_4 = new ProcessBuilder("sudo", "nmap", "localhost");
+        String port_status = "";
+
+         try {
+            Process process_4 = processBuilder_4.start();
+            int exitCode = process_4.waitFor();
+
+            if (exitCode == 0) {
+                port_status = readOutput(process_4.getInputStream());
+            } else {
+                port_status = "Port scan error.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred while retrieving Apache2 status.";
+        }
+  //sudo service mysql status
+
+        ProcessBuilder processBuilder_5 = new ProcessBuilder("sudo", "service", "mysql", "status");
+        String database_status = "";
+
+         try {
+            Process process_5 = processBuilder_5.start();
+            int exitCode = process_5.waitFor();
+
+            if (exitCode == 0) {
+                database_status = readOutput(process_5.getInputStream());
+            } else {
+                  database_status = "Database status error.";
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return "An error occurred while retrieving database status.";
+        }
+        //
+         //
         model.addAttribute("processInfo", processOutput.toString());
+        model.addAttribute("highlightedLines", highlightedLines);
+        model.addAttribute("highlightedLines_NODEJS",    highlightedLines_NODEJS);
+        model.addAttribute("apacheStatus",    apache2_status);
+        model.addAttribute("nginxStatus",    nginx_status);
+        model.addAttribute("portStatus",    port_status);
+        model.addAttribute("databaseStatus",    database_status);
+
 
 
 
         return "upload";
     }
+
+    
 
     // Method to list files in the current directory
     private List<String> listFilesInCurrentDirectory() {
@@ -232,5 +389,26 @@ public class FileUploadController {
             }
         }
         return fileNames;
+    }
+
+        @GetMapping("/delete/{fileName}")
+    public String deleteFile(@PathVariable String fileName, Model model) {
+        String currentWorkingDir = System.getProperty("user.dir");
+        String filePath = currentWorkingDir +"/" +  fileName;
+        System.out.println(filePath);
+        File file = new File(filePath);
+        /*
+         * 
+         */
+        if (file.exists()) {
+            try{
+                file.delete();
+
+            }catch(Exception e){
+                System.out.println("Error : " + e);
+            }
+            //fileList.remove(fileName);
+        }
+        return "redirect:/";
     }
 }
